5157595bc45b74c9148b3c7b3264bf841d0de04a
osbm <osmanfbayram@gmail.com>
2025-03-02 00:34:18 +0300

    fill out hash

--- hosts/pochita/configuration.nix
+++ hosts/pochita/configuration.nix
@@ -50,7 +50,7 @@
         # update time to time
         # last update: 2025-03-02
         plugins = [ "github.com/caddy-dns/cloudflare@1fb64108d4debf196b19d7398e763cb78c8a0f41" ];
-        hash = "";
+        hash = "sha256-3nvVGW+ZHLxQxc1VCc/oTzCLZPBKgw4mhn+O3IoyiSs=";
       };
     email = "contact@osbm.dev";
     config = ''
