7d7e2eec37debd2fcb01780434aff20115df3050
osbm <osmanfbayram@gmail.com>
2025-03-04 02:05:17 +0300

    lets try this

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -35,18 +35,7 @@
       ];
 
       systemd.services.caddy.serviceConfig = {
-        LoadCredential = "CLOUDFLARE_API_TOKEN:${config.age.secrets.cloudflare.path}";
-        EnvironmentFile = "-%t/caddy/secrets.env";
-        RuntimeDirectory = "caddy";
-        ExecStartPre = [
-          ((pkgs.writeShellApplication {
-              name = "caddy-secrets";
-              text = "echo \"CLOUDFLARE_API_TOKEN=\\\"$(<\"$CREDENTIALS_DIRECTORY/CLOUDFLARE_API_TOKEN\")\\\"\" > \"$RUNTIME_DIRECTORY/secrets.env\"";
-            })
-            + "/bin/caddy-secrets")
-        ];
-        AmbientCapabilities = "cap_net_bind_service";
-        CapabilityBoundingSet = "cap_net_bind_service";
+        EnvironmentFile = "/etc/caddy/.env";
       };
     })
   ];
