994be0d3e41b6598396d606f95423179072609e3
osbm <osmanfbayram@gmail.com>
2025-03-08 00:53:44 +0300

    disable remote builds for now

--- modules/remote-builds.nix
+++ modules/remote-builds.nix
@@ -1,18 +1,19 @@
 {
-  config,
-  outputs,
+#  config,
+#  outputs,
   ...
 }: {
-  nix.distributedBuilds = true;
-  nix.settings.builders-use-substitutes = true;
-  nix.buildMachines = [
-    {
-      hostName = "ymir.curl-boga.ts.net";
-      systems = ["x86_64-linux" "aarch64-linux"];
-      supportedFeatures = outputs.nixosConfigurations.ymir.config.nix.settings.system-features;
-      sshKey = config.age.secrets.ssh-key-private.path;
-      sshUser = "osbm";
-      protocol = "ssh-ng";
-    }
-  ];
+
+  # nix.distributedBuilds = true;
+  # nix.settings.builders-use-substitutes = true;
+  # nix.buildMachines = [
+  #  {
+  #   hostName = "ymir.curl-boga.ts.net";
+  #    systems = ["x86_64-linux" "aarch64-linux"];
+  #    supportedFeatures = outputs.nixosConfigurations.ymir.config.nix.settings.system-features;
+  #    sshKey = config.age.secrets.ssh-key-private.path;
+  #    sshUser = "osbm";
+  #    protocol = "ssh-ng";
+  #  }
+  # ];
 }
