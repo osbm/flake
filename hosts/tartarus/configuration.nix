98bebc15a24f8523203c3eeefb17e99cd910cd2d
osbm <osmanfbayram@gmail.com>
2025-03-07 03:52:46 +0300

    remove nixd

--- hosts/tartarus/configuration.nix
+++ hosts/tartarus/configuration.nix
@@ -43,9 +43,8 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
     inputs.osbm-nvim.packages.x86_64-linux.default
-    nixd
   ];
 
   system.stateVersion = "24.05"; # lalalalala
--- hosts/ymir/configuration.nix
+++ hosts/ymir/configuration.nix
@@ -94,7 +94,6 @@
   environment.systemPackages = with pkgs; [
     nvidia-container-toolkit
     inputs.osbm-nvim.packages.x86_64-linux.default
-    nixd
   ];
 
   system.stateVersion = "25.05"; # great taboo of the nixos world
