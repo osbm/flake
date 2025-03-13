14a116b18be1893480666837e5ce8799a08ce92f
osbm <osmanfbayram@gmail.com>
2025-03-13 18:02:01 +0300

    much closer to the turth now

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -2,10 +2,10 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      font.size = 12;
+      font.size = 12.0;
       terminal.shell = {
         args = ["new-session" "-A" "-s" "general"];
-        program = pkgs.tmux;
+        program = pkgs.tmux + "/bin/tmux";
       };
       window = {
         decorations = "None";
