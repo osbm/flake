c6927a28e2c119cdf8da8acb082fe5841d1970d1
osbm <osmanfbayram@gmail.com>
2025-03-13 20:21:44 +0300

    add starship settings

--- home/starship.nix
+++ home/starship.nix
@@ -2,5 +2,11 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      add_newline = false;
+      dart.disabled = true;
+      python.disabled = true;
+      nodejs.disabled = true;
+    };
   };
 }
