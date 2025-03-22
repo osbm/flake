203f0bbc6f096c416c7d83c875ee0bc04618b723
osbm <osmanfbayram@gmail.com>
2025-03-22 21:40:34 +0300

    try this instead

--- modules/minegrub.nix
+++ modules/minegrub.nix
@@ -2,11 +2,12 @@
   config,
   inputs,
   lib,
+  pkgs,
   ...
 }: {
-  imports = [
-    inputs.minegrub-theme.nixosModules.default
-  ];
+  # imports = [
+  #   inputs.minegrub-theme.nixosModules.default
+  # ];
   options = {
     myModules.enableMinegrubTheme = lib.mkOption {
       type = lib.types.bool;
@@ -17,12 +18,20 @@
   config = lib.mkMerge [
     (lib.mkIf config.myModules.enableMinegrubTheme {
       boot.loader.grub = {
-        minegrub-theme = {
-          enable = true;
-          splash = "100% Flakes!";
-          background = "background_options/1.8  - [Classic Minecraft].png";
-          boot-options-count = 4;
-        };
+        # minegrub-theme = {
+        #   enable = true;
+        #   splash = "100% Flakes!";
+        #   background = "background_options/1.8  - [Classic Minecraft].png";
+        #   boot-options-count = 4;
+        # };
+        theme =
+          pkgs.fetchFromGitHub
+          {
+            owner = "Lxtharia";
+            repo = "minegrub-theme";
+            rev = "193b3a7c3d432f8c6af10adfb465b781091f56b3";
+            sha256 = "1bvkfmjzbk7pfisvmyw5gjmcqj9dab7gwd5nmvi8gs4vk72bl2ap";
+          };
       };
     })
   ];
