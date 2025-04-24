68fe79641b23c963535ad5a572a431abaada82b3
osbm <osmanfbayram@gmail.com>
2025-04-24 22:06:06 +0300

    dont forget to import home manager

--- modules/home.nix
+++ modules/home.nix
@@ -1,8 +1,12 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.osbm = import ../home/home.nix {
