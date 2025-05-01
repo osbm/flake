8e7f771ee99979d0be03716400e952e400d99156
osbm <osmanfbayram@gmail.com>
2025-05-01 23:43:12 +0300

    rekey for our beloved new server

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,7 @@ let
   ymir = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgjjvukAG0RvQfHj5Iy64XOFh9YbdnNAmgFUvzlnAEt";
   tartarus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxbIyQnQFA1RFQKH4eHHWcT7Z0tCumerCsRMjlHgSPd";
   pochita = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHpE9pf7ZeNvpW1GxLLF8kB0Q8HQO7XSIea1Oe9qubKt";
+  wallfacer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOr8pQvLsNCHQdsBKWpziYTPjBkEcQy272kZ5Gqoaatt";
 
   osbm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfnV+qqUCJf92npNW4Jy0hIiepCJFBDJHXBHnUlNX0k";
 
@@ -9,6 +10,7 @@ let
     ymir
     tartarus
     pochita
+    wallfacer
   ];
 in {
   "network-manager.age".publicKeys = machines ++ [osbm];
