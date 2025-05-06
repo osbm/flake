8c795262536f851ee3d6a19eaa5b8a5140d1159c
osbm <osmanfbayram@gmail.com>
2025-05-06 10:00:56 +0300

    just give reviews

--- home/tmux/wanikani-current-reviews.sh
+++ home/tmux/wanikani-current-reviews.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 #!nix-shell -i bash -p jq curl
-
 get_wanikani()
 {
     wanikani_reviews=$(curl -s -H "Authorization: Bearer 2da24e4a-ba89-4c4a-9047-d08f21e9dd01" "https://api.wanikani.com/v2/assignments?immediately_available_for_review=true" | jq '.total_count')
-    wanikani_lessons=$(curl -s -H "Authorization: Bearer 2da24e4a-ba89-4c4a-9047-d08f21e9dd01" "https://api.wanikani.com/v2/assignments?immediately_available_for_lessons=true" | jq '.total_count')
-
-    echo "$wanikani_lessons lessons $wanikani_reviews reviews"
+    echo "$wanikani_reviews reviews"
 }
 
 main()
 {
     get_wanikani
-    # sleep for 10 minutes
     sleep 600
 }
 
