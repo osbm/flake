98d2b5732ec63853d5fb7718166f9d52b319988d
osbm <osmanfbayram@gmail.com>
2025-05-17 00:38:33 +0300

    formatting

--- modules/services/wanikani-fetch-data/default.nix
+++ modules/services/wanikani-fetch-data/default.nix
@@ -1,19 +1,21 @@
-{pkgs, config, lib, ...}:
-let
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
   wanikani-fetcher = pkgs.writeShellApplication {
     name = "wanikani-fetcher";
     runtimeInputs = with pkgs; [curl jq zip];
-    # read script from the wanikani-fetcher.sh file
     text = builtins.readFile ./wanikani-fetcher.sh;
   };
-in  {
+in {
   options.services.wanikani-fetch-data.enable = lib.mkEnableOption {
     description = "Enable WaniKani Fetch Data";
     default = false;
   };
 
   config = lib.mkIf config.services.wanikani-fetch-data.enable {
-
     systemd.timers.wanikani-fetch-data = {
       description = "WaniKani Fetch Data";
       wantedBy = ["timers.target"];
