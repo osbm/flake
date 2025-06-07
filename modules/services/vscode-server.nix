242800a5ae8ceb87050e681f31329e4f60b65cb6
osbm <osmanfbayram@gmail.com>
2025-06-07 20:31:23 +0300

    update the user to see if i can use my own files with this

--- modules/services/vscode-server.nix
+++ modules/services/vscode-server.nix
@@ -4,6 +4,7 @@
     port = 4444;
     disableTelemetry = true;
     disableUpdateCheck = true;
+    user = "osbm";
     host = "localhost";
     hashedPassword = "$argon2i$v=19$m=4096,t=3,p=1$dGc0TStGMDNzSS9JRkJYUFp3d091Q2p0bXlzPQ$zvdE9BkclkJmyFaenzPy2E99SEqsyDMt4IQNZfcfFFQ";
   };
