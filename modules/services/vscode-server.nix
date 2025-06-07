e221828bba748822f636a9ed344ac602863f17db
osbm <osmanfbayram@gmail.com>
2025-06-07 20:29:31 +0300

    enable the vscode port

--- modules/services/vscode-server.nix
+++ modules/services/vscode-server.nix
@@ -1,4 +1,4 @@
-{
+{config, ...}:{
   services.code-server = {
     enable = true;
     port = 4444;
@@ -7,4 +7,5 @@
     host = "localhost";
     hashedPassword = "$argon2i$v=19$m=4096,t=3,p=1$dGc0TStGMDNzSS9JRkJYUFp3d091Q2p0bXlzPQ$zvdE9BkclkJmyFaenzPy2E99SEqsyDMt4IQNZfcfFFQ";
   };
+  networking.firewall.allowedTCPPorts = [config.services.code-server.port];
 }
