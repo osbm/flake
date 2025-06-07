390e36c8342419b1d2e7d353eb4424f43db26f23
osbm <osmanfbayram@gmail.com>
2025-06-07 21:08:28 +0300

    this looks a bit smart ngl

--- modules/services/vscode-server.nix
+++ modules/services/vscode-server.nix
@@ -5,7 +5,7 @@
     disableTelemetry = true;
     disableUpdateCheck = true;
     user = "osbm";
-    host = "localhost";
+    host = "${config.networking.hostName}.curl-boga.ts.net";
     hashedPassword = "$argon2i$v=19$m=4096,t=3,p=1$dGc0TStGMDNzSS9JRkJYUFp3d091Q2p0bXlzPQ$zvdE9BkclkJmyFaenzPy2E99SEqsyDMt4IQNZfcfFFQ";
   };
   networking.firewall.allowedTCPPorts = [config.services.code-server.port];
