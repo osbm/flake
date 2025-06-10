508ea51a8f4d5f79873e16c9ae05b3d90fe8357f
osbm <osmanfbayram@gmail.com>
2025-06-10 23:01:19 +0300

    add group

--- modules/services/vscode-server.nix
+++ modules/services/vscode-server.nix
@@ -5,6 +5,8 @@
     disableTelemetry = true;
     disableUpdateCheck = true;
     user = "osbm";
+    group = "users";
+    # auth = "none";
     host = "${config.networking.hostName}.curl-boga.ts.net";
     hashedPassword = "$argon2i$v=19$m=4096,t=3,p=1$dGc0TStGMDNzSS9JRkJYUFp3d091Q2p0bXlzPQ$zvdE9BkclkJmyFaenzPy2E99SEqsyDMt4IQNZfcfFFQ";
   };
