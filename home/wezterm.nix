654644140013fa58cdc7fc9ae3a904d1ef1e2a46
osbm <osmanfbayram@gmail.com>
2025-06-22 11:58:12 +0300

    try new config

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -8,7 +8,12 @@
     programs.wezterm = {
       enable = config.enableWezterm;
       extraConfig = ''
+        _G.shells = {
+          fish = ${lig.getExe pkgs.fish}
+        };
 
+        local cfg = require('config');
+        return cfg
       '';
     };
   };
