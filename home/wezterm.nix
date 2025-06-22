a083a92fe52f0fa4daaec4d5cc5978d8fe3966a1
osbm <osmanfbayram@gmail.com>
2025-06-23 00:30:45 +0300

    try fullscreen

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -12,6 +12,13 @@
         _G.shells = {
           fish = '${lib.getExe pkgs.fish}'
         };
+
+        wezterm.on('gui-startup', function(cmd)
+          local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+          window:gui_window():maximize()
+        end)
+
+
         return {
           default_prog = { _G.shells.fish },
           window_decorations = "NONE",
