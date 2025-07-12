7ce780986beb87b48ea341e1c60e890a23b619dc
osbm <osmanfbayram@gmail.com>
2025-07-12 21:35:19 +0300

    add lib

---
+++ lib/default.nix
@@ -0,0 +1,7 @@
+{pkgs, ...}: {
+
+  importList = lib.mapAttrsToList (name: _path: ./. + "/${name}")
+    (lib.filterAttrs (filename: kind:
+      filename != "default.nix" && (kind == "regular" || kind == "directory"))
+    (builtins.readDir ./.));
+}
