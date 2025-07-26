0b551af8ef60d7a4ac7c1b239cf119c962f0bc8d
osbm <osbm@osbm.dev>
2025-07-27 02:51:16 +0300

    add hydra to wallfacer

--- hosts/wallfacer/configuration.nix
+++ hosts/wallfacer/configuration.nix
@@ -9,10 +9,19 @@
     enableFonts = false;
     enableNextcloud = true;
   };
+
+  services.hydra = {
+    enable = true;
+    hydraURL = "http://localhost:54543";
+    notificationSender = "hydra@localhost";
+    buildMachinesFiles = [];
+    useSubstitutes = true;
+  };
+  networking.firewall.allowedTCPPorts = [ 54543 ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   i18n.inputMethod.enable = lib.mkForce false;
   networking.hostName = "wallfacer";
-  # services.getty.autologinUser = "osbm";
   system.stateVersion = "25.05";
 }
