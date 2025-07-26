5dee47dc2383e09762805332d61656c7978015ff
osbm <osmanfbayram@gmail.com>
2025-07-26 12:30:53 +0300

    try this why this shit doesnt work

--- home/gpg.nix
+++ home/gpg.nix
@@ -4,11 +4,11 @@
     enable = true;
     enableFishIntegration = true;
     enableSshSupport = true;
-    extraConfig = ''
-      allow-loopback-pinentry
-    '';
-    pinentry.program = "pinentry-cursor";
+    # extraConfig = ''
+    #   allow-loopback-pinentry
+    # '';
+    pinentry.package = pkgs.pinentry-tty;
   };
   programs.gpg.enable = true;
-  home.packages = [ pkgs.pinentry-curses ];
+  # home.packages = [ pkgs.pinentry-curses ];
 }
