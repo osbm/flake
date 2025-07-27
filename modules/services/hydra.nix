a349b7a389b6ed6fc6437adc5a14500d7997891e
osbm <osbm@osbm.dev>
2025-07-27 17:16:20 +0300

    fix hydra url

--- modules/services/hydra.nix
+++ modules/services/hydra.nix
@@ -17,7 +17,7 @@
       services.hydra = {
         enable = true;
         port = 3000;
-        hydraURL = "http://localhost:${toString config.services.hydra.port}";
+        hydraURL = "http://${config.networking.hostName}.curl-boga.ts.net/hydra/";
         notificationSender = "hydra@localhost";
         buildMachinesFiles = [];
         useSubstitutes = true;
