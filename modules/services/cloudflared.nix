b475086cb704f9acb9fba132ac8eac881f3ad6b0
osbm <osbm@osbm.dev>
2025-07-27 21:13:54 +0300

    add tunnel

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -17,19 +17,26 @@
       services.cloudflared = {
         enable = true;
         certificateFile = "/home/osbm/.cloudflared/cert.pem";
-        tunnels."eb9052aa-9867-482f-80e3-97a7d7e2ef04" = {
-          default = "http_status:404";
-          credentialsFile = "/home/osbm/.cloudflared/eb9052aa-9867-482f-80e3-97a7d7e2ef04.json";
-          ingress = {
-            "git.osbm.dev" = {
-              service = "http://localhost:3000";
+        tunnels = {
+          "eb9052aa-9867-482f-80e3-97a7d7e2ef04" = {
+            default = "http_status:404";
+            credentialsFile = "/home/osbm/.cloudflared/eb9052aa-9867-482f-80e3-97a7d7e2ef04.json";
+            ingress = {
+              "git.osbm.dev" = {
+                service = "http://localhost:3000";
+              };
+            };
+          };
+          "288d47fb-95ee-4019-9e07-6f918ab02357" = {
+            default = "http_status:404";
+            credentialsFile = "/home/osbm/.cloudflared/eb9052aa-9867-482f-80e3-97a7d7e2ef04.json";
+            ingress = {
+              "git.osbm.dev" = {
+                service = "http://localhost:3000";
+              };
             };
           };
         };
-      };
-      boot.kernel.sysctl = {
-        "net.core.rmem_max" = 7500000;
-        "net.core.wmem_max" = 7500000;
       };
     })
   ];
