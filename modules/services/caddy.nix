4d63dc34391dd6699f39154ccbe776909139f873
osbm <osbm@osbm.dev>
2025-07-27 17:29:28 +0300

    this is the stupitest shit ever

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -21,7 +21,7 @@
           # update time to time
           # last update: 2025-03-02
           plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de" ];
-          hash = "sha256-YYpsf8HMONR1teMiSymo2y+HrKoxuJMKIea5/NEykGc=";
+          hash = "sha256-x5SJSns16VaqOyKLCtaMIMWaooT+C0f9iSWKFBTywnA=";
         };
         email = "osbm@osbm.dev";
         extraConfig = ''
