9464f20626a336bbff58bdba65bb66dd82d85b09
osbm <osbm@osbm.dev>
2025-07-27 15:48:40 +0300

    update email

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -23,7 +23,7 @@
           plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de" ];
           hash = "sha256-YYpsf8HMONR1teMiSymo2y+HrKoxuJMKIea5/NEykGc=";
         };
-        email = "contact@osbm.dev";
+        email = "osbm@osbm.dev";
         extraConfig = ''
           (cloudflare) {
             tls {
