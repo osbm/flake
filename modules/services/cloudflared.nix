0024bd9c915505521c681c69ec6d32a2fc657512
osbm <osbm@osbm.dev>
2025-07-27 21:19:09 +0300

    create token

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -29,7 +29,7 @@
           };
           "288d47fb-95ee-4019-9e07-6f918ab02357" = {
             default = "http_status:404";
-            credentialsFile = "/home/osbm/.cloudflared/eb9052aa-9867-482f-80e3-97a7d7e2ef04.json";
+            credentialsFile = "/home/osbm/.cloudflared/288d47fb-95ee-4019-9e07-6f918ab02357.json";
             ingress = {
               "git.osbm.dev" = {
                 service = "http://localhost:3000";
