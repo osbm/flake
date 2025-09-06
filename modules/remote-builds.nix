bfbddcf2757b24e54757128f732cbe1c17a109c1
osbm <osbm@osbm.dev>
2025-09-06 15:59:33 +0300

    enable remote builds

--- modules/remote-builds.nix
+++ modules/remote-builds.nix
@@ -4,16 +4,16 @@
   ...
 }:
 {
-  # nix.distributedBuilds = true;
+  nix.distributedBuilds = true;
   # nix.settings.builders-use-substitutes = true;
-  # nix.buildMachines = [
-  #  {
-  #   hostName = "ymir.curl-boga.ts.net";
-  #    systems = ["x86_64-linux" "aarch64-linux"];
-  #    supportedFeatures = outputs.nixosConfigurations.ymir.config.nix.settings.system-features;
-  #    sshKey = config.age.secrets.ssh-key-private.path;
-  #    sshUser = "osbm";
-  #    protocol = "ssh-ng";
-  #  }
-  # ];
+  nix.buildMachines = [
+   {
+    hostName = "ymir.curl-boga.ts.net";
+     systems = ["x86_64-linux" "aarch64-linux"];
+     supportedFeatures = outputs.nixosConfigurations.ymir.config.nix.settings.system-features;
+     sshKey = config.age.secrets.ssh-key-private.path;
+     sshUser = "osbm";
+     protocol = "ssh-ng";
+   }
+  ];
 }
