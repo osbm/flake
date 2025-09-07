018f4da4ad38bafc7d66dff320da655e04bae428
osbm <osbm@osbm.dev>
2025-09-07 10:57:51 +0300

    update remote builds

--- modules/remote-builds.nix
+++ modules/remote-builds.nix
@@ -1,19 +1,38 @@
 {
    config,
    outputs,
+   lib,
   ...
 }:
 {
-  nix.distributedBuilds = true;
-  # nix.settings.builders-use-substitutes = true;
-  nix.buildMachines = [
-   {
-    hostName = "ymir";
-     systems = ["x86_64-linux" "aarch64-linux"];
-     supportedFeatures = outputs.nixosConfigurations.ymir.config.nix.settings.system-features;
-     sshKey = config.age.secrets.ssh-key-private.path;
-     sshUser = "osbm";
-     protocol = "ssh-ng";
-   }
+  config = lib.mkMerge [
+    (lib.mkIf (config.networking.hostName != "ymir") {
+      nix.distributedBuilds = true;
+      # nix.settings.builders-use-substitutes = true;
+      nix.buildMachines = [
+        {
+          hostName = "ymir";
+          systems = ["x86_64-linux" "aarch64-linux"];
+          supportedFeatures = outputs.nixosConfigurations.ymir.config.nix.settings.system-features;
+          sshKey = config.age.secrets.ssh-key-private.path;
+          sshUser = "osbm";
+          protocol = "ssh-ng";
+        }
+      ];
+    })
+    (lib.mkIf (config.networking.hostName != "wallfacer") {
+      nix.distributedBuilds = true;
+      # nix.settings.builders-use-substitutes = true;
+      nix.buildMachines = [
+        {
+          hostName = "wallfacer";
+          systems = ["x86_64-linux"];
+          supportedFeatures = outputs.nixosConfigurations.wallfacer.config.nix.settings.system-features;
+          sshKey = config.age.secrets.ssh-key-private.path;
+          sshUser = "osbm";
+          protocol = "ssh-ng";
+        }
+      ];
+    })
   ];
 }
