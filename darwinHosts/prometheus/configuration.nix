0a2c94924d6916a9de05f8e787cd7b0719003a7e
osbm <osbm@osbm.dev>
2025-09-09 14:48:39 +0300

    fuck

--- darwinHosts/prometheus/configuration.nix
+++ darwinHosts/prometheus/configuration.nix
@@ -2,7 +2,6 @@
   imports = [
     # ../../modules
     ../../modules/common-packages.nix
-    ../../modules/nix-settings.nix
     inputs.home-manager.darwinModules.home-manager
     ./dummy-module.nix
   ];
@@ -20,6 +19,71 @@
       enableGTK = false;
     };
   };
+  # Allow unfree packages
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "vscode" # TODO: remove this
+      "discord"
+      "obsidian"
+      "steam"
+      "steam-unwrapped"
+      "open-webui"
+      "vscode-extension-github-copilot"
+      "spotify"
+      "cursor"
+      # nvidia related (i have to)
+      "nvidia-x11"
+      "cuda_cudart"
+      "libcublas"
+      "cuda_cccl"
+      "cuda_nvcc"
+      "nvidia-settings"
+      "cuda-merged"
+    ];
+  nixpkgs.config.allowAliases = false;
+
+  # enable nix flakes
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+  nix.channel.enable = false;
+
+  nix.registry = {
+    self.flake = inputs.self;
+    nixpkgs.flake = inputs.nixpkgs;
+    osbm-nvim.flake = inputs.osbm-nvim;
+    my-nixpkgs.to = {
+      owner = "osbm";
+      repo = "nixpkgs";
+      type = "github";
+    };
+    osbm-dev.to = {
+      owner = "osbm";
+      repo = "osbm.dev";
+      type = "github";
+    };
+    devshells.to = {
+      owner = "osbm";
+      repo = "devshells";
+      type = "github";
+    };
+  };
+
+  nix.settings.trusted-users = [
+    "root"
+    "osbm"
+    "mac" # TODO change mac user to osbm
+  ];
+
+
+  # nix.nixPath = ["nixpkgs=${pkgs.path}"];
+
+  system.configurationRevision = inputs.self.rev or "dirty";
 
   programs.fish.enable = true;
 
