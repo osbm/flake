e6911401c7f6e54c65027efea0272bd1a67c5b36
osbm <osbm@osbm.dev>
2025-09-09 11:21:01 +0300

    how about this

--- modules/minegrub.nix
+++ modules/minegrub.nix
@@ -16,22 +16,20 @@
       description = "Enable Minecraft theme for grub.";
     };
   };
-  config = lib.mkMerge [
-    (lib.mkIf config.myModules.enableMinegrubTheme {
-      boot.loader.grub = {
-        # minegrub-theme = {
-        #   enable = true;
-        #   splash = "100% Flakes!";
-        #   background = "background_options/1.8  - [Classic Minecraft].png";
-        #   boot-options-count = 4;
-        # };
-        theme = pkgs.fetchFromGitHub {
-          owner = "Lxtharia";
-          repo = "minegrub-theme";
-          rev = "193b3a7c3d432f8c6af10adfb465b781091f56b3";
-          sha256 = "1bvkfmjzbk7pfisvmyw5gjmcqj9dab7gwd5nmvi8gs4vk72bl2ap";
-        };
+  config = lib.mkIf config.myModules.enableMinegrubTheme {
+    boot.loader.grub = {
+      # minegrub-theme = {
+      #   enable = true;
+      #   splash = "100% Flakes!";
+      #   background = "background_options/1.8  - [Classic Minecraft].png";
+      #   boot-options-count = 4;
+      # };
+      theme = pkgs.fetchFromGitHub {
+        owner = "Lxtharia";
+        repo = "minegrub-theme";
+        rev = "193b3a7c3d432f8c6af10adfb465b781091f56b3";
+        sha256 = "1bvkfmjzbk7pfisvmyw5gjmcqj9dab7gwd5nmvi8gs4vk72bl2ap";
       };
-    })
-  ];
+    };
+  };
 }
