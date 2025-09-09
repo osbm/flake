f1aa15495daf114b336c8f5c35585db43c41b58a
osbm <osbm@osbm.dev>
2025-09-09 11:32:33 +0300

    try

--- darwinHosts/prometheus/configuration.nix
+++ darwinHosts/prometheus/configuration.nix
@@ -1,14 +1,29 @@
 {pkgs, ...}: {
   imports = [
-    ../../modules
+    # ../../modules
+    ../../modules/common-packages.nix
+    ../../modules/home.nix
+    ../../modules/nix-settings.nix
+    ../../modules/secrets.nix
+    ../../modules/i18n.nix
   ];
+
   myModules.setUsers = false;
   users.users.mac = {
     description = "mac";
     shell = pkgs.fish;
     home = "/Users/mac";
   };
-
+  environment.systemPackages = with pkgs; [
+    alacritty
+    ghostty
+    kitty
+    vscode
+    blender
+    libreoffice
+    code-cursor
+    ungoogled-chromium
+  ];
   system.stateVersion = 6;
   nixpkgs.hostPlatform = "x86_64-darwin";
 }
