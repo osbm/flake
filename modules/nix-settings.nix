d770dc53653c5c0fb2da8461ab218766e912e5a5
osbm <osbm@osbm.dev>
2025-09-17 10:09:53 +0300

    fix syntax error

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -75,7 +75,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "main:2AjPdIsbKyoTGuw+4x2ZXMUT/353CXosW9pdbTQtjqw="
     ];
-  }
+  };
 
   nix.gc = {
     automatic = true;
