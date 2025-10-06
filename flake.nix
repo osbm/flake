b6cbc17a3f7e47fa2a28b31789204570cee4c644
osbm <osbm@osbm.dev>
2025-10-06 20:37:27 +0300

    clean

--- flake.nix
+++ flake.nix
@@ -43,8 +43,10 @@
       url = "github:serokell/deploy-rs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
