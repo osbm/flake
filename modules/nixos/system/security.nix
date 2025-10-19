f5d6d62278dba3f7fbcc6152db68389bc3263c2c
osbm <osbm@osbm.dev>
2025-10-19 16:53:45 +0300

    u

--- modules/nixos/system/security.nix
+++ modules/nixos/system/security.nix
@@ -3,10 +3,10 @@
   config = lib.mkIf config.osbmModules.security.enable {
     # Security hardening
     security.sudo.wheelNeedsPassword = lib.mkDefault true;
-    
+
     # Polkit for privilege escalation
     security.polkit.enable = lib.mkDefault true;
-    
+
     # Additional security settings can be added here
   };
 }
