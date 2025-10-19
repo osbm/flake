852a363b04c1714fa5534a839e2f0f7c0e861998
osbm <osbm@osbm.dev>
2025-10-19 18:39:02 +0300

    simpler

--- modules/nixos/system/home-manager.nix
+++ modules/nixos/system/home-manager.nix
@@ -1,9 +1,11 @@
 { lib, config, inputs, ... }:
-let
-  cfg = config.osbmModules;
-in
+
 {
-  config = lib.mkIf (cfg.homeManager.enable && inputs ? home-manager) {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  config = lib.mkIf (config.osbmModules.homeManager.enable && inputs ? home-manager) {
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
@@ -12,7 +14,7 @@ in
       extraSpecialArgs = { inherit inputs; };
 
       # Configure home-manager for each user
-      users = lib.genAttrs cfg.users (username: {
+      users = lib.genAttrs config.osbmModules.users (username: {
         home.stateVersion = lib.mkDefault "24.05";
         imports = [ ../../home-manager ];
       });
