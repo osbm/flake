ddd2d05c433749bab51a467a6d23f26d1fddd603
osbm <osbm@osbm.dev>
2025-10-19 20:49:04 +0300

    nix index

--- modules/nixos/system/nix-index.nix
+++ modules/nixos/system/nix-index.nix
@@ -1,5 +1,9 @@
 { lib, config, inputs, ... }:
 {
+  imports = [
+    inputs.nix-index-database.nixosModules.nix-index
+  ];
+
   config = lib.mkIf (config.osbmModules.nixIndex.enable && inputs ? nix-index-database) {
     programs.nix-index-database.comma.enable = true;
     programs.command-not-found.enable = false;
