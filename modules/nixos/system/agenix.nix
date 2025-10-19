7dfddb9fee72c83897b16e5fabde50255d38c05c
osbm <osbm@osbm.dev>
2025-10-19 18:39:05 +0300

    fix

--- modules/nixos/system/agenix.nix
+++ modules/nixos/system/agenix.nix
@@ -1,10 +1,32 @@
-{ lib, config, ... }:
+{ lib, pkgs, inputs, config, ... }:
 {
+  imports = [
+    inputs.agenix.nixosModules.default
+  ];
+
   config = lib.mkIf config.osbmModules.agenix.enable {
-    # Agenix will be configured via the agenix input
-    # This module exists to enable agenix-related configurations
-    age.identityPaths = lib.mkDefault [
-      "/etc/ssh/ssh_host_ed25519_key"
-    ];
+      environment.systemPackages = [
+        inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.agenix
+        pkgs.age
+      ];
+
+      age.secrets = {
+        network-manager.file = ../secrets/network-manager.age;
+        ssh-key-private = {
+          file = ../secrets/ssh-key-private.age;
+          path = "/home/osbm/.ssh/id_ed25519";
+          owner = "osbm";
+          group = "users";
+          mode = "600";
+        };
+        ssh-key-public = {
+          file = ../secrets/ssh-key-public.age;
+          path = "/home/osbm/.ssh/id_ed25519.pub";
+          owner = "osbm";
+          group = "users";
+          mode = "644";
+        };
+      };
+
   };
 }
