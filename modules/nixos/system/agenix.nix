5788098c170c6fd15350fc53c126e15d943f570b
osbm <osbm@osbm.dev>
2025-10-21 21:29:13 +0300

    fix agenix

--- modules/nixos/system/agenix.nix
+++ modules/nixos/system/agenix.nix
@@ -17,16 +17,16 @@
     ];
 
     age.secrets = {
-      network-manager.file = ../secrets/network-manager.age;
+      network-manager.file = ../../../secrets/network-manager.age;
       ssh-key-private = {
-        file = ../secrets/ssh-key-private.age;
+        file = ../../../secrets/ssh-key-private.age;
         path = "/home/osbm/.ssh/id_ed25519";
         owner = "osbm";
         group = "users";
         mode = "600";
       };
       ssh-key-public = {
-        file = ../secrets/ssh-key-public.age;
+        file = ../../../secrets/ssh-key-public.age;
         path = "/home/osbm/.ssh/id_ed25519.pub";
         owner = "osbm";
         group = "users";
