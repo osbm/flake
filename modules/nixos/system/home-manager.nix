88f7974e6aafe9429cbf36b2606bf023d0ca886e
osbm <osbm@osbm.dev>
2025-10-21 08:38:19 +0300

    home manager impermanence things

--- modules/nixos/system/home-manager.nix
+++ modules/nixos/system/home-manager.nix
@@ -6,11 +6,11 @@
 }:
 
 {
-  imports = lib.optionals (inputs ? home-manager) [
+  imports = [
     inputs.home-manager.nixosModules.home-manager
   ];
 
-  config = lib.mkIf (config.osbmModules.homeManager.enable && inputs ? home-manager) {
+  config = lib.mkIf (config.osbmModules.homeManager.enable) {
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
@@ -21,7 +21,32 @@
       # Configure home-manager for each user (excluding root)
       users = lib.genAttrs (builtins.filter (u: u != "root") config.osbmModules.users) (username: {
         home.stateVersion = lib.mkDefault "24.05";
-        imports = [ ../../home-manager ];
+        imports = [ 
+          ../../home-manager 
+        ] ++ lib.optionals config.osbmModules.hardware.disko.zfs.impermanence.enable [
+          # if impermanence is enabled, configure persistence
+          {
+            home.persistence."/persist/home/${username}" = {
+              directories = [
+                "Pictures"
+                "Documents"
+                "Videos"
+                ".gnupg"
+                ".ssh"
+                ".local/share/keyrings"
+                ".local/share/direnv"
+                # {
+                #   directory = ".local/share/Steam";
+                #   method = "symlink";
+                # }
+              ];
+              files = [
+                ".screenrc"
+              ];
+              allowOther = true;
+            };
+          }
+        ];
       });
     };
   };
