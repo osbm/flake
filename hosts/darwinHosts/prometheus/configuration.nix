a8e7aebcdee992f398187330eb2079cba268da95
osbm <osbm@osbm.dev>
2025-10-21 16:33:25 +0300

    how about this

--- hosts/darwinHosts/prometheus/configuration.nix
+++ hosts/darwinHosts/prometheus/configuration.nix
@@ -9,6 +9,8 @@
   imports = [
     ../../../modules/nixos/options.nix
     ../../../modules/nixos/programs/command-line.nix
+    ../../../modules/nixos/programs/neovim.nix
+    ../../../modules/nixos/system/nix-settings.nix
     inputs.home-manager.darwinModules.home-manager
   ];
 
@@ -30,107 +32,6 @@
   services.tailscale = {
     enable = true;
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-    builtins.elem (lib.getName pkg) [
-      "vscode" # TODO: remove this
-      "discord"
-      "obsidian"
-      "steam"
-      "steam-unwrapped"
-      "open-webui"
-      "vscode-extension-github-copilot"
-      "spotify"
-      "cursor"
-      # nvidia related (i have to)
-      "nvidia-x11"
-      "cuda_cudart"
-      "libcublas"
-      "cuda_cccl"
-      "cuda_nvcc"
-      "nvidia-settings"
-      "cuda-merged"
-    ];
-  nixpkgs.config.allowAliases = false;
-
-  # enable nix flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  # nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-
-  nix.channel.enable = false;
-
-  nix.registry = {
-    self.flake = inputs.self;
-    nixpkgs.flake = inputs.nixpkgs;
-    # nixpkgs = {
-    #   from = { type = "indirect"; id = "nixpkgs"; };
-    #   to = {
-    #     path = inputs.nixpkgs.outPath;
-    #     type = "path";
-    #   };
-    # };
-
-    # Commented out because i want to make sure it works if i switch a system
-    # to another nixpkgs with a different flake input name
-    # nixpkgs = {
-    #   from = { type = "indirect"; id = "nixpkgs"; };
-    #   to = {
-    #     path = pkgs.path;
-    #     type = "path";
-    #   };
-    # };
-
-    osbm-nvim.flake = inputs.osbm-nvim;
-    my-nixpkgs = {
-      from = {
-        type = "indirect";
-        id = "my-nixpkgs";
-      };
-      to = {
-        owner = "osbm";
-        repo = "nixpkgs";
-        type = "github";
-      };
-    };
-    osbm-dev = {
-      from = {
-        type = "indirect";
-        id = "osbm-dev";
-      };
-      to = {
-        owner = "osbm";
-        repo = "osbm.dev";
-        type = "github";
-      };
-    };
-    devshells = {
-      from = {
-        type = "indirect";
-        id = "devshells";
-      };
-      to = {
-        owner = "osbm";
-        repo = "devshells";
-        type = "github";
-      };
-    };
-  };
-
-  nix.settings.trusted-users = [
-    "root"
-    "osbm"
-    "mac" # TODO change mac user to osbm
-  ];
-
-  # nix.nixPath = ["nixpkgs=${pkgs.path}"];
-
-  system.configurationRevision = inputs.self.rev or "dirty";
 
   programs.fish.enable = true;
 
