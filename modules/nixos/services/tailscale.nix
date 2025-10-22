53c234ec3c8ed1663cc1163a3542f1554a21b27c
osbm <osbm@osbm.dev>
2025-10-22 13:12:55 +0300

    clean

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -5,13 +5,6 @@
   ...
 }:
 {
-
-  # i have 4 machines, 2 of them are always at home
-  # pochita (raspberry pi 5) and ymir (desktop)
-  # pochita will be on all the time, ymir can be wake on lan
-
-  # and i have a laptop named tartarus
-
   config = lib.mkMerge [
     (lib.mkIf config.osbmModules.services.tailscale.enable {
       services.tailscale = {
