32d69d95372cfda643445f3135bea0477da174c5
osbm <osbm@osbm.dev>
2025-10-22 13:12:32 +0300

    just use the default package

--- modules/nixos/services/cloudflare-dyndns.nix
+++ modules/nixos/services/cloudflare-dyndns.nix
@@ -4,34 +4,10 @@
   pkgs,
   ...
 }:
-let
-  # https://github.com/NixOS/nixpkgs/pull/394352
-  cloudflare-dyndns-5-3 = pkgs.cloudflare-dyndns.overridePythonAttrs rec {
-    version = lib.warnIfNot (
-      pkgs.cloudflare-dyndns.version == "5.0"
-    ) "The cloudflare-dyndns package is updated, you should remove this override" "5.3";
-    src = pkgs.fetchFromGitHub {
-      owner = "kissgyorgy";
-      repo = "cloudflare-dyndns";
-      rev = "v${version}";
-      hash = "sha256-t0MqH9lDfl+cAnPYSG7P32OGO8Qpo1ep0Hj3Xl76lhU=";
-    };
-    build-system = with pkgs.python3Packages; [
-      hatchling
-    ];
-    dependencies = with pkgs.python3Packages; [
-      click
-      httpx
-      pydantic
-      truststore
-    ];
-  };
-in
 {
   config = lib.mkMerge [
     (lib.mkIf config.osbmModules.services.cloudflare-dyndns.enable {
       services.cloudflare-dyndns = {
-        package = cloudflare-dyndns-5-3;
         enable = true;
         apiTokenFile = "/persist/cloudflare-dyndns";
         proxied = false; # TODO please revert
