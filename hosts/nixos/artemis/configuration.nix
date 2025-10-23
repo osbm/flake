d7b69e36c3ceacbb7491831a6af3fc80208de07e
osbm <osbm@osbm.dev>
2025-10-23 21:31:17 +0300

    artemis config

--- hosts/nixos/artemis/configuration.nix
+++ hosts/nixos/artemis/configuration.nix
@@ -6,58 +6,14 @@
 {
   imports = [
     (import "${inputs.mobile-nixos}/lib/configuration.nix" { device = "oneplus-enchilada"; })
+    ../../../modules/nixos
   ];
 
-  # Allow unfree packages (needed for OnePlus firmware)
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable SSH server (essential for mobile device access)
-  services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "yes"; # For initial setup
-  services.openssh.settings.PasswordAuthentication = true; # For initial setup
-
-  # Set root password for SSH access
-  users.users.root.password = "nixtheplanet";
-
-  # Enable GNOME Desktop Environment
-  services.xserver.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.enable = true;
-
-  # Enable GNOME Keyring for password management
-  services.gnome.gnome-keyring.enable = true;
-
-  # Enable dconf for GNOME settings
-  programs.dconf.enable = true;
-
-  # Remove unwanted GNOME applications
-  environment.gnome.excludePackages = with pkgs; [
-    baobab # disk usage analyzer
-    cheese # photo booth
-    eog # image viewer
-    epiphany # web browser
-    simple-scan # document scanner
-    totem # video player
-    yelp # help viewer
-    evince # document viewer
-    file-roller # archive manager
-    geary # email client
-    seahorse # password manager
-    gnome-calculator
-    gnome-calendar
-    gnome-characters
-    gnome-clocks
-    gnome-contacts
-    gnome-font-viewer
-    gnome-logs
-    gnome-maps
-    gnome-music
-    gnome-screenshot
-    gnome-system-monitor
-    gnome-weather
-    gnome-disk-utility
-    pkgs.gnome-connections
-  ];
+  osbmModules = {
+    desktopEnvironment = "gnome";
+    machineType = "mobile";
+    hardware.systemd-boot.enable = false; # Mobile devices use different bootloader
+  };
 
   # Minimal essential packages
   environment.systemPackages = with pkgs; [
