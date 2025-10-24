0fdc1b1c9ca8910c1902dca1ab4e1636102c25f5
osbm <osbm@osbm.dev>
2025-10-24 08:37:36 +0300

    add impermanent folders for tailscale

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -15,5 +15,18 @@
       networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
       environment.systemPackages = [ pkgs.tailscale ];
     })
+
+    # tailscale and impermanence
+    (lib.mkIf
+      (
+        config.osbmModules.services.tailscale.enable
+        && config.osbmModules.hardware.disko.zfs.root.impermanenceRoot
+      )
+      {
+        environment.persistence."/persist".directories = [
+          "/var/lib/tailscale"
+        ];
+      }
+    )
   ];
 }
