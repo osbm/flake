05da6e9cecd2a2101f8b9b7925c93610f57278f7
osbm <osbm@osbm.dev>
2025-10-28 19:20:26 +0300

    fix anubis empty module

--- modules/nixos/services/anubis.nix
+++ modules/nixos/services/anubis.nix
@@ -7,7 +7,7 @@
   config = lib.mkMerge [
     (lib.mkIf config.osbmModules.services.anubis.enable {
       services.anubis = {
-        enable = true;
+        # enable = true;
       };
     })
   ];
