93c243b0d35804e0d49b1dfb786b2cb294827e42
osbm <osbm@osbm.dev>
2025-11-05 14:51:36 +0300

    add global shell aliases

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -13,6 +13,17 @@
     EDITOR = lib.mkDefault "nvim";
   };
 
+  home.shellAliases = {
+    c = "code .";
+    l = "eza --all --long --git --icons --group --sort size --header --group-directories-first";
+    ll = "eza --all --long --git --icons --group --sort name --header --group-directories-first";
+    free = "free -h";
+    df = "df -h";
+    du = "du -h";
+    lg = "lazygit";
+    onefetch = "onefetch -T prose -T programming -T data";
+  };
+
   # Don't set stateVersion here - let it be set by the system configuration
   # home.stateVersion should be set in the system's home-manager configuration
 
--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -4,16 +4,6 @@
     interactiveShellInit = ''
       set -g fish_greeting
     '';
-    shellAliases = {
-      c = "code .";
-      l = "eza --all --long --git --icons --group --sort size --header --group-directories-first";
-      ll = "eza --all --long --git --icons --group --sort name --header --group-directories-first";
-      free = "free -h";
-      df = "df -h";
-      du = "du -h";
-      lg = "lazygit";
-      onefetch = "onefetch -T prose -T programming -T data";
-    };
     functions = {
       gitu = ''
         git add --all
