7137c32889e30dbf3f9621440f7dbf032dfefd83
osbm <osbm@osbm.dev>
2025-11-05 14:10:57 +0300

    stop nginx for a sec

--- hosts/nixos/apollo/configuration.nix
+++ hosts/nixos/apollo/configuration.nix
@@ -12,7 +12,7 @@
       glance.enable = true;
       # anubis.enable = true;
       # mailserver.enable = true;
-      nginx.enable = true;
+      # nginx.enable = true;
       forgejo.enable = true;
       vaultwarden.enable = true;
       immich.enable = true;
