5671c7997d5a7ee0d2c6d530854b761b298b4806
osbm <osbm@osbm.dev>
2025-11-07 10:16:08 +0300

    group tartarus

--- hosts/nixos/tartarus/hardware-configuration.nix
+++ hosts/nixos/tartarus/hardware-configuration.nix
@@ -9,16 +9,18 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "nvme"
-    "usb_storage"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "nvme"
+      "usb_storage"
+      "sd_mod"
+    ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/246d3df7-3578-44b2-8aee-c1ed33581184";
