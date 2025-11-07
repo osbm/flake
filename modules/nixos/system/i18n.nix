cef8b3ee81d7fbf74ee40e21bc8c2db770ec5b97
osbm <osbm@osbm.dev>
2025-11-07 10:16:40 +0300

    group i18n

--- modules/nixos/system/i18n.nix
+++ modules/nixos/system/i18n.nix
@@ -9,19 +9,31 @@
     time.timeZone = lib.mkDefault "Europe/Istanbul";
 
     # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
 
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "tr_TR.UTF-8";
-      LC_IDENTIFICATION = "tr_TR.UTF-8";
-      LC_MEASUREMENT = "tr_TR.UTF-8";
-      LC_MONETARY = "tr_TR.UTF-8";
-      LC_NAME = "tr_TR.UTF-8";
-      LC_NUMERIC = "tr_TR.UTF-8";
-      LC_PAPER = "tr_TR.UTF-8";
-      LC_TELEPHONE = "tr_TR.UTF-8";
-      LC_TIME = "ja_JP.UTF-8";
-      # LC_ALL = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = "tr_TR.UTF-8";
+        LC_IDENTIFICATION = "tr_TR.UTF-8";
+        LC_MEASUREMENT = "tr_TR.UTF-8";
+        LC_MONETARY = "tr_TR.UTF-8";
+        LC_NAME = "tr_TR.UTF-8";
+        LC_NUMERIC = "tr_TR.UTF-8";
+        LC_PAPER = "tr_TR.UTF-8";
+        LC_TELEPHONE = "tr_TR.UTF-8";
+        LC_TIME = "ja_JP.UTF-8";
+        # LC_ALL = "en_US.UTF-8";
+      };
+
+      inputMethod = {
+        type = "fcitx5";
+        enable = config.osbmModules.desktopEnvironment != "none";
+        fcitx5.addons = with pkgs; [
+          fcitx5-mozc
+          fcitx5-gtk
+          fcitx5-nord # a color theme
+        ];
+      };
     };
 
     services.xserver.xkb = {
@@ -29,14 +41,5 @@
       variant = "";
     };
 
-    i18n.inputMethod = {
-      type = "fcitx5";
-      enable = config.osbmModules.desktopEnvironment != "none";
-      fcitx5.addons = with pkgs; [
-        fcitx5-mozc
-        fcitx5-gtk
-        fcitx5-nord # a color theme
-      ];
-    };
   };
 }
