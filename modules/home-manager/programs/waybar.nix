31f7bcc54834fe2994f3b977e4caaa84fcccf3b7
osbm <osbm@osbm.dev>
2025-12-21 19:37:39 +0300

    dont add waybar everywhere

--- modules/home-manager/programs/waybar.nix
+++ modules/home-manager/programs/waybar.nix
@@ -1,6 +1,16 @@
 {
-  programs.waybar = {
-    enable = true;
-    systemd.enable = true;
-  };
+  lib,
+  nixosConfig,
+  ...
+}:
+{
+  config = lib.mkMerge [
+    # Auto-enable Waybar only if system has a desktop environment
+    (lib.mkIf (nixosConfig != null && !nixosConfig.osbmModules.desktopEnvironment.none) {
+      programs.waybar = {
+        enable = lib.mkDefault true;
+        systemd.enable = true;
+      };
+    })
+  ];
 }
