35a29b77892b7a70812b0bcd54fbbf1abf27b7bd
osbm <osbm@osbm.dev>
2025-12-22 15:07:30 +0300

    lets try this to enable stuff on prometheus

--- modules/home-manager/programs/swww.nix
+++ modules/home-manager/programs/swww.nix
@@ -1,19 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages = [ pkgs.swww ];
+  config = lib.mkIf pkgs.stdenv.isLinux {
+    home.packages = [ pkgs.swww ];
 
-  systemd.user.services.swww = {
-    Unit = {
-      Description = "Wayland wallpaper daemon";
-      PartOf = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
+    systemd.user.services.swww = {
+      Unit = {
+        Description = "Wayland wallpaper daemon";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.swww}/bin/swww-daemon";
+        Restart = "on-failure";
+      };
+
+      Install.WantedBy = [ "graphical-session.target" ];
     };
-
-    Service = {
-      ExecStart = "${pkgs.swww}/bin/swww-daemon";
-      Restart = "on-failure";
-    };
-
-    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
--- modules/home-manager/programs/waybar.nix
+++ modules/home-manager/programs/waybar.nix
@@ -1,12 +1,13 @@
 {
   lib,
+  pkgs,
   nixosConfig,
   ...
 }:
 {
   config = lib.mkMerge [
-    # Auto-enable Waybar only if system has a desktop environment
-    (lib.mkIf (nixosConfig != null && !nixosConfig.osbmModules.desktopEnvironment.none) {
+    # Auto-enable Waybar only if system has a desktop environment and is Linux
+    (lib.mkIf (pkgs.stdenv.isLinux && nixosConfig != null && !nixosConfig.osbmModules.desktopEnvironment.none) {
       programs.waybar = {
         enable = lib.mkDefault true;
         systemd.enable = true;
