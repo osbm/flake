db613dee5842d3a4065974c9ecf7d1738724d387
osbm <osbm@osbm.dev>
2026-01-20 08:13:35 +0300

    use

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -10,7 +10,7 @@
         git commit -m "$argv"
         git push
       '';
-      yy = ''
+      y = ''
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
         command yazi $argv --cwd-file="$tmp"
         if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
