151caaced53603abea17a7041a57e5bb8ebb7714
osbm <osbm@osbm.dev>
2026-01-20 08:11:05 +0300

    add fish command

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -10,6 +10,14 @@
         git commit -m "$argv"
         git push
       '';
+      yy = ''
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+          builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+      '';
     };
   };
 }
