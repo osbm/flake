c5de3f13676eb46bb2bf7fb585f67fe414eea020
osbm <osbm@osbm.dev>
2026-01-30 13:39:16 +0300

    add wallfacer and pochita

--- modules/nixos/services/syncthing.nix
+++ modules/nixos/services/syncthing.nix
@@ -25,10 +25,10 @@
           };
           devices = {
             wallfacer = {
-              id = "WALLFACER-DEVICE-ID"; # Replace with actual ID from wallfacer
+              id = "LNPYIG4-LG2TCG5-IFCUQ7B-BVPGGR3-2C7TRI5-PN2DNYJ-EIXGINX-LUAUPQE";
             };
             pochita = {
-              id = "POCHITA-DEVICE-ID"; # Replace with actual ID from pochita
+              id = "5YCBKWJ-A33M6MH-XY6E6HO-QNDQE7N-PEVRL5I-6SQPQMX-563J5JN-C5FAPAU";
             };
             luoji = {
               id = "54O4Q42-GXACXO6-BK7TF4Q-NVBW6OF-ODPPVWO-WLW43CV-ZZEZXQD-JUAF7AY";
