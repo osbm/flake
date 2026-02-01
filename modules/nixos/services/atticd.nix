cab2baebc21973202f34d136b4539ddccc90df10
osbm <osbm@osbm.dev>
2026-02-01 18:04:58 +0300

    remove cloudflared

--- modules/nixos/services/atticd.nix
+++ modules/nixos/services/atticd.nix
@@ -27,17 +27,6 @@ in
         };
       };
       networking.firewall.allowedTCPPorts = [ atticdPort ];
-      services.cloudflared.tunnels = {
-        "fa301a21-b259-4149-b3d0-b1438c7c81f8" = {
-          default = "http_status:404";
-          credentialsFile = "/home/osbm/.cloudflared/fa301a21-b259-4149-b3d0-b1438c7c81f8.json";
-          ingress = {
-            "cache.osbm.dev" = {
-              service = "http://localhost:${toString atticdPort}";
-            };
-          };
-        };
-      };
     })
   ];
 }
