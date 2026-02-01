ce2d46357543e4541893725a9e71e3405a130faf
osbm <osbm@osbm.dev>
2026-02-01 17:37:42 +0300

    add proxy

--- modules/nixos/services/nginx.nix
+++ modules/nixos/services/nginx.nix
@@ -8,6 +8,25 @@
     (lib.mkIf config.osbmModules.services.nginx.enable {
       services.nginx = {
         enable = true;
+
+        # Route [name].sync.osbm.dev -> [name].curl-boga.ts.net:8384
+        appendHttpConfig = ''
+          server {
+            listen 80;
+            server_name ~^(?<device>.+)\.sync\.osbm\.dev$;
+
+            location / {
+              proxy_pass http://$device.curl-boga.ts.net:8384;
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_http_version 1.1;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+            }
+          }
+        '';
       };
 
       networking.firewall.allowedTCPPorts = [
