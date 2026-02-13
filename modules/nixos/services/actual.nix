e994b4fec0be6639d14076dfead1b2c758b46a21
osbm <osbm@osbm.dev>
2026-02-13 09:40:00 +0300

    try this

--- modules/nixos/services/actual.nix
+++ modules/nixos/services/actual.nix
@@ -7,9 +7,21 @@
         enable = true;
         settings = {
           port = 51514;
-
-          # dataDir = "/var/lib/actual"
         };
+      };
+
+      # disable DynamicUser, use a static user instead
+      users.users.actual = {
+        isSystemUser = true;
+        group = "actual";
+        home = "/var/lib/actual";
+      };
+      users.groups.actual = { };
+
+      systemd.services.actual.serviceConfig = {
+        DynamicUser = lib.mkForce false;
+        User = "actual";
+        Group = "actual";
       };
     })
 
@@ -25,7 +37,24 @@
       };
     })
 
-    # impermanence: actual data lives in /var/lib/private/actual (DynamicUser),
-    # persisted via /var/lib/private in base impermanence config
+    # impermanence with actual
+    (lib.mkIf
+      (
+        config.osbmModules.services.actual.enable
+        && config.osbmModules.hardware.disko.zfs.root.impermanenceRoot
+      )
+      {
+        environment.persistence."/persist" = {
+          directories = [
+            {
+              directory = "/var/lib/actual";
+              user = "actual";
+              group = "actual";
+              mode = "0750";
+            }
+          ];
+        };
+      }
+    )
   ];
 }
