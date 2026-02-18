64f868268450acc3db025c284518d304b262f191
osbm <osbm@osbm.dev>
2026-02-18 10:00:41 +0300

    add darwin support

--- modules/nixos/system/nix-index.nix
+++ modules/nixos/system/nix-index.nix
@@ -1,16 +1,25 @@
 {
   lib,
   config,
+  pkgs,
   inputs,
   ...
 }:
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+  nixIndexModule =
+    if isDarwin then
+      inputs.nix-index-database.darwinModules.nix-index
+    else
+      inputs.nix-index-database.nixosModules.nix-index;
+in
 {
   imports = [
-    inputs.nix-index-database.nixosModules.nix-index
+    nixIndexModule
   ];
 
   config = lib.mkIf (config.osbmModules.nixIndex.enable && inputs ? nix-index-database) {
     programs.nix-index-database.comma.enable = true;
-    programs.command-not-found.enable = false;
+    programs.command-not-found.enable = lib.mkIf (!isDarwin) false;
   };
 }
