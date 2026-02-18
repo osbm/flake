3d8a30a6072873c6b489ccc2646467a9cd97b6ff
osbm <osbm@osbm.dev>
2026-02-18 22:16:05 +0300

    add

--- modules/nixos/system/desktop-environment.nix
+++ modules/nixos/system/desktop-environment.nix
@@ -14,6 +14,7 @@ in
       services = {
         xserver.enable = true;
         displayManager.sddm.enable = true;
+        displayManager.sddm.wayland.enable = true;
         desktopManager.plasma6.enable = true;
         printing.enable = true;
       };
