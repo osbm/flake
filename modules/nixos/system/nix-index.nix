cbf2775b3e870fa3c46d96379bb7dbe12c5a19f3
osbm <osbm@osbm.dev>
2026-02-18 10:07:13 +0300

    Revert "add darwin support"
    This reverts commit 64f868268450acc3db025c284518d304b262f191.

--- modules/nixos/system/nix-index.nix
+++ modules/nixos/system/nix-index.nix
@@ -1,25 +1,16 @@
 {
   lib,
   config,
-  pkgs,
   inputs,
   ...
 }:
-let
-  isDarwin = pkgs.stdenv.isDarwin;
-  nixIndexModule =
-    if isDarwin then
-      inputs.nix-index-database.darwinModules.nix-index
-    else
-      inputs.nix-index-database.nixosModules.nix-index;
-in
 {
   imports = [
-    nixIndexModule
+    inputs.nix-index-database.nixosModules.nix-index
   ];
 
   config = lib.mkIf (config.osbmModules.nixIndex.enable && inputs ? nix-index-database) {
     programs.nix-index-database.comma.enable = true;
-    programs.command-not-found.enable = lib.mkIf (!isDarwin) false;
+    programs.command-not-found.enable = false;
   };
 }
