127349c748e493f23988ecd8a130fb661959b6ad
osbm <osbm@osbm.dev>
2026-03-25 13:47:48 +0300

    fix

--- modules/nixos/services/mailserver.nix
+++ modules/nixos/services/mailserver.nix
@@ -107,9 +107,9 @@
             }
             # Mail folder
             {
-              directory = config.mailserver.mailDirectory; # /var/vmail by default
-              user = config.mailserver.vmailUserName;
-              group = config.mailserver.vmailGroupName;
+              directory = config.mailserver.storage.path; # /var/vmail by default
+              user = config.mailserver.storage.owner;
+              group = config.mailserver.storage.group;
               mode = "0700";
             }
             # DKIM is used to sign outgoing emails to verify they are from the claimed domain.
