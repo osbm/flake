271dc0dcf6df29004c8baa63fdf7ad41429775c5
osbm <osbm@osbm.dev>
2026-09-08 22:06:13 +0200

    luoji sync proxy

--- modules/nixos/services/nginx.nix
+++ modules/nixos/services/nginx.nix
@@ -36,6 +36,11 @@
 
         # Route [name].sync.osbm.dev -> [name].curl-boga.ts.net:8384
         appendHttpConfig = ''
+          # devices whose syncthing GUI forces TLS (self-signed) get https upstream
+          map $device $syncthing_scheme {
+            default "http";
+            luoji "https";
+          }
           server {
             listen 80;
             listen 443 ssl;
@@ -47,8 +52,9 @@
             resolver 100.100.100.100;
 
             location / {
-              set $backend http://$device.curl-boga.ts.net:8384;
+              set $backend $syncthing_scheme://$device.curl-boga.ts.net:8384;
               proxy_pass $backend;
+              proxy_ssl_verify off;
               proxy_set_header Host $host;
               proxy_set_header X-Real-IP $remote_addr;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
